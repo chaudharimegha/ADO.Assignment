9931e75b33b675871a61a5671c9b2a5b961b9ff0
chaudharimegha <117818003+chaudharimegha@users.noreply.github.com>
2022-12-04 23:22:02 +0530

    Add files via upload

---
+++ FootBallController.cs
@@ -0,0 +1,38 @@
+﻿using FootBallLeage.Data;
+using FootBallLeage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootBallLeage.Controllers
+{
+    public class FootBallController : Controller
+    {
+        private readonly DBContext _context;
+        public FootBallController(DBContext context)
+        { 
+            _context=context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<FootBallModel> FootBallObj = _context.FootBallLeages ;
+            return View(FootBallObj);
+        }
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(FootBallModel footBallModel)
+    {
+            if (ModelState.IsValid)
+            {
+                _context.FootBallLeages.Add(footBallModel);
+                _context.SaveChanges();
+                TempData["ResultOk"] = "Record Added Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(footBallModel);
+    }
+    }
+}
---
+++ StoreFootBallDataController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+namespace FootBallLeage.Controllers
+{
+    public class StoreFootBallDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
